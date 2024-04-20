4d126d7be9cc134febc1e466acf49fbc8ff141e7
Bagus Ario Yudanto <bagusarioyudanto123@gmail.com>
2024-04-20 09:25:21 +0700

    feature(supplier): membuat tampilan tambah data supplier

---
+++ src/custom_palette/RoundedPanel.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package custom_palette;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.beans.BeanProperty;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Bagus
+ */
+public class RoundedPanel extends JPanel {
+    private int cornerRadius = 15;
+
+    public RoundedPanel() {
+        super();
+        setOpaque(false); // Make the panel transparent
+    }
+
+    public RoundedPanel(int radius) {
+        super();
+        cornerRadius = radius;
+        setOpaque(false); // Make the panel transparent
+    }
+    
+    @BeanProperty
+    public void setCornerRadius(int cornerRadius) {
+        this.cornerRadius = cornerRadius;
+        repaint(); // Memastikan panel digambar ulang setelah perubahan
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Dimension arcs = new Dimension(cornerRadius, cornerRadius);
+        int width = getWidth();
+        int height = getHeight();
+        Graphics2D graphics = (Graphics2D) g.create();
+        graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        // Draws the rounded panel with borders.
+        graphics.setColor(getBackground());
+        graphics.fillRoundRect(0, 0, width-1, height-1, arcs.width, arcs.height); //paint background
+        graphics.setColor(getForeground());
+        graphics.drawRoundRect(0, 0, width-1, height-1, arcs.width, arcs.height); //paint border
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(200, 100); // Set the preferred size of the panel
+    }
+
+    @Override
+    public Dimension getMinimumSize() {
+        return new Dimension(100, 50); // Set the minimum size of the panel
+    }
+}
---
+++ src/custom_palette/RoundedTextField.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package custom_palette;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.beans.BeanProperty;
+import javax.swing.BorderFactory;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author Bagus
+ */
+public class RoundedTextField extends JTextField {
+    private Color lineColor;
+    private int strokeWidth;
+    private int cornerRadius = 15;
+
+    public RoundedTextField() {
+        lineColor = new Color(52, 152, 219);
+        strokeWidth = 2;
+        setOpaque(false);
+        setBorder(BorderFactory.createEmptyBorder(2, 10, 2, 10));
+    }
+
+    public Color getLineColor() {
+        return lineColor;
+    }
+
+    public void setLineColor(Color lineColor) {
+        this.lineColor = lineColor;
+    }
+
+    public int getStrokeWidth() {
+        return strokeWidth;
+    }
+
+    public void setStrokeWidth(int strokeWidth) {
+        this.strokeWidth = strokeWidth;
+    }
+
+    public int getCornerRadius() {
+        return cornerRadius;
+    }
+
+    public void setCornerRadius(int cornerRadius) {
+        this.cornerRadius = cornerRadius;
+        repaint(); // Pastikan komponen digambar ulang setelah perubahan
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        if (!isOpaque()) {
+            Graphics2D g2d = (Graphics2D) g;
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            int s = strokeWidth;
+            int w = getWidth() - (2 * s);
+            int h = getHeight() - (2 * s);
+            g2d.setColor(getBackground());
+            g2d.fillRoundRect(s, s, w, h, cornerRadius, cornerRadius);
+            g2d.setStroke(new BasicStroke(s));
+            g2d.setColor(lineColor);
+            g2d.drawRoundRect(s, s, w, h, cornerRadius, cornerRadius);
+        }
+        super.paintComponent(g);
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(150, 22); // Atur ukuran preferensi komponen
+    }
+
+    @Override
+    public Dimension getMinimumSize() {
+        return new Dimension(100, 22); // Atur ukuran minimum komponen
+    }
+}
